a09c1003558a90a16046b9006dd42c0390d8e74c
Peter Masucci <peter.a.masucci@gmail.com>
2019-05-22 17:34:30 -0400

    add roms and implement codes through 0xD

--- src/unchip.rs
+++ src/unchip.rs
@@ -23,7 +23,134 @@ pub fn unchip(rom: &str) {
 }
 
 fn read_instruction(buffer: &[u8], pc: usize) {
-  let code = [buffer[pc], buffer[pc + 1]];
+  let word = (buffer[pc] as u16) << 8 | buffer[pc + 1] as u16;
+  let instruction = find_opcode(word);
+  println!(
+    "{:0>5x} {:02x} {:02x} {}",
+    pc << 8,
+    word >> 8,
+    word & 0x00FF,
+    instruction
+  );
+}
 
-  println!("{:0<4x} {:x} {:x}", pc, code[0], code[1]);
+fn find_opcode(code: u16) -> String {
+  let first_nibble = code >> 12;
+  let instruction = match first_nibble {
+    0x0 => match code & 0x00ff {
+      0xE0 => "CLS".to_string(),
+      0xEE => "RET".to_string(),
+      _ => "UNIMPLEMENTED".to_string(),
+    },
+    0x1 => format!("{:<13} {:#x}", "JP", code & 0x0fff),
+    0x2 => format!("{:<13} {:>#05x}", "CALL", code & 0x0fff),
+    0x3 => format!(
+      "{:<9} V{:X}, {:>#05x}",
+      "SE",
+      (code & 0x0f00) >> 8,
+      code & 0x00ff
+    ),
+    0x4 => format!(
+      "{:<9} V{:X}, {:>#05x}",
+      "SNE",
+      (code & 0x0f00) >> 8,
+      code & 0x00ff
+    ),
+    0x5 => format!(
+      "{:<9} V{:X}, V{:X}",
+      "SE",
+      (code & 0x0f00) >> 8,
+      (code & 0x00f0) >> 4
+    ),
+    0x6 => format!(
+      "{:<9} V{:X}, {:>#05x}",
+      "LD",
+      (code & 0x0f00) >> 8,
+      code & 0x00ff
+    ),
+    0x7 => format! {"{:<9} V{:X}, {:>#05x}", "ADD", (code & 0x0f00) >> 8, code & 0x00ff},
+    0x8 => match code & 0x000f {
+      0x0 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "LD",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0x1 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "OR",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0x2 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "AND",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0x3 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "XOR",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0x4 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "ADD.",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0x5 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "SUB.",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0x6 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "SHR.",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0x7 => format!(
+        "{:<9} V{:X}, V{:X}",
+        "SUBN.",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      0xE => format!(
+        "{:<9} V{:X}, V{:X}",
+        "SHL.",
+        (code & 0x0f00) >> 8,
+        (code & 0x00f0) >> 4
+      ),
+      _ => "UNIMPLEMENTED".to_string(),
+    },
+    0x9 => format!(
+      "{:<9} V{:X}, V{:X}",
+      "SNE",
+      (code & 0x0f00) >> 8,
+      (code & 0x00f0) >> 4
+    ),
+    0xA => format!("{:<9}  I, {:>#05x}", "LD", code & 0x0fff),
+    0xB => format!("{:<9} V0, {:>#05x}", "JP", code & 0x0fff),
+    0xC => format!(
+      "{:<9} V{:X}, {:>#05x}",
+      "RND",
+      (code & 0x0f00) >> 8,
+      (code & 0x00ff)
+    ),
+    0xD => format!(
+      "{:<5} V{:X}, V{:X}, {:>#05x}",
+      "DRW",
+      (code & 0x0f00) >> 8,
+      (code & 0x00f0) >> 4,
+      code & 0x000f
+    ),
+    // 0xE => "0xE SKIPs not implemented yet",
+    // 0xF => "0xF functions not implemented yet",
+    _ => "instruction not recognized".to_string(),
+  };
+
+  instruction.to_string()
 }
